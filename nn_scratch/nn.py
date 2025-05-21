7e132aa373f71a8b57fdab356bf829a58fc042d4
yamisukii <merdian.stefan@gmail.com>
2025-05-21 11:31:23 +0200

    added functionalties

--- nn_scratch/nn.py
+++ nn_scratch/nn.py
@@ -1,4 +1,5 @@
 import numpy as np
+import utils
 
 
 class NeuralNetwork:
@@ -9,12 +10,34 @@ class NeuralNetwork:
         self.activations = activations
 
     def _init_weights(self):
-        # Initialize weights and biases
-        ...
+        params = {}
+        for i in range(len(self.layer_sizes) - 1):
+            input_size = self.layer_sizes[i]
+            output_size = self.layer_sizes[i + 1]
+
+            # generating random weights
+            W = utils.generate_wt(input_size, output_size)
+            b = np.zeros((1, output_size))
+
+            params[f"W{i+1}"] = W
+            params[f"b{i+1}"] = b
 
     def forward(self, X):
-        # Compute layer outputs
-        ...
+        self.cache = {}  # store z and a values for backprop
+        a = X
+
+        for i in range(1, len(self.layer_sizes)):
+            w = self.params[f"W{i}"]
+            b = self.params[f"b{i}"]
+
+            z = np.dot(a, w) + b
+            self.cache[f"Z{i}"] = z
+
+            activation_fn = self.activations[i - 1]
+            a = activation_fn(z)
+            self.cache[f"A{i}"] = a
+
+        return a
 
     def backward(self, X, y, outputs):
         # Compute gradients
--- nn_scratch/utils.py
+++ nn_scratch/utils.py
@@ -1,14 +1,29 @@
 import numpy as np
 
 
-def relu(x): return np.maximum(0, x)
-def relu_derivative(x): return (x > 0).astype(float)
+def relu(x):
+    return np.maximum(0, x)
 
 
-def sigmoid(x): return 1 / (1 + np.exp(-x))
-def sigmoid_derivative(x): return sigmoid(x) * (1 - sigmoid(x))
+def relu_derivative(x):
+    return (x > 0).astype(float)
+
+
+def sigmoid(x):
+    return (1/(1 + np.exp(-x)))
+
+
+def sigmoid_derivative(x):
+    return sigmoid(x) * (1 - sigmoid(x))
 
 
 def softmax(x):
     exps = np.exp(x - np.max(x, axis=1, keepdims=True))
     return exps / np.sum(exps, axis=1, keepdims=True)
+
+
+def generate_wt(x, y):
+    li = []
+    for i in range(x * y):
+        li.append(np.random.randn())
+    return (np.array(li).reshape(x, y))
